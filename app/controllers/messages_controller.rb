0e6a1d2664259a8935e5af9c459fccae49ef656e
Nick Phillips <phillipn101@gmail.com>
2017-03-27 11:49:26 -0500

    add match index functionality

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,10 @@
 class MessagesController < ApplicationController
+  before_action :check_if_logged_in
+
   def create
     @message = Message.new(message_params)
     @message.sender_id = session[:user]['id']
-    @message.match_room_id = 
+    @message.match_room_id = params[:id]
 
     if @message.save
       flash[:notice] = ['Success']
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'match_rooms/' => 'match_rooms#index', as: "match_room_index"
   get 'match_rooms/:id/' => 'match_rooms#show', as: "show_match_room"
   post 'match_rooms/' => 'match_rooms#create', as: "create_match_room"
+  put 'match_rooms/:id' => 'match_rooms#update', as: "update_match_room"
   post 'match_rooms/:id/messages/'  => 'messages#create', as: "create_message"
 
 
