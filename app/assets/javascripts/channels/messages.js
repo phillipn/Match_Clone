a50e935ec2aa8b9021dadd37600de8f2934c25e8
Nick Phillips <phillipn101@gmail.com>
2017-03-27 18:36:04 -0500

    add cool js

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,12 +1,11 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
-    console.log('received');
     $("#messages").removeClass('hidden')
-    return $('#messages').append(this.renderMessage(data));
+    $('#messages').append(this.renderMessage(data));
+    $("#messages").scrollTop(100000)
   },
 
   renderMessage: function(data) {
-    console.log(data.message);
-    return "<div class='panel-header'><h3>" + data.sender + "<small>at " + data.message.created_at + "</small></h3></div><div class='panel-body'><p>" + data.message.content + "</p></div>";
+    return "<div class='panel-header'><h3>" + data.sender + "<small> at " + data.message.created_at + "</small></h3></div><div class='panel-body'><p>" + data.message.content + "</p></div>";
   }
 });
---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $("#messages").height($(window).height() - "200px");
+  $("#messages").scrollTop(100000)
+})
