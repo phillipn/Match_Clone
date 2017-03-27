afed37c9b590f4c0391ade21b2d016a41f8529dc
Nick Phillips <phillipn101@gmail.com>
2017-03-26 19:29:55 -0500

    revise user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_one :location
-  has_one :match
+
+  has_one :active_match, -> { includes :receiver }, class_name: 'Match', foreign_key: 'sender_id'
+  has_one :active_love, class_name: 'User', through: 'active_match', source: 'receiver'
+
+  has_one :passive_match, -> { includes :sender }, class_name: 'Match', foreign_key: 'receiver_id'
+  has_one :passive_love, class_name: 'User', through: 'passive_match', source: 'sender'
 
   has_many :sent_messages, -> { includes :receiver }, class_name: 'Message', foreign_key: 'sender_id'
   has_many :sent, class_name: 'User', through: 'sent_messages', source: 'receiver'
