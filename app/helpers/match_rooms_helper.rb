b3838dc786d70a402c05d57644cc856f9e1b0b4d
Nick Phillips <phillipn101@gmail.com>
2017-03-27 13:27:50 -0500

    add helper method for show action of match_room

--- app/helpers/match_rooms_helper.rb
+++ app/helpers/match_rooms_helper.rb
@@ -1,2 +1,11 @@
 module MatchRoomsHelper
+  def chat_room_greeting(sender, receiver)
+    if sender.id == session[:user]['id']
+      sender = 'you'
+    else
+      receiver = 'you'
+    end
+
+    return "Chat between #{sender} and #{receiver}"
+  end
 end
