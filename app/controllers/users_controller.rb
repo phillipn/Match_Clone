8d70b033e95a14789a31c87acc5cfda5ae30bc1a
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-03-27 11:04:04 -0500

    user routes and view created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def index
+    
+  end
+  def new
+    
+  end
+  def create
+    
+  end
+  def login
+    
+  end
+  def logout
+    
+  end
+  def show
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  get '/users/new' => 'users#new'
+  post'/users' => 'users#create'
+  post '/login' => 'users#login'
+  post '/logout' => 'users#logout'
+  get '/users/:id' => 'users#show'
+
   get 'match_rooms/:id/' => 'match_rooms#show', as: "show_match_room"
   post 'match_rooms/' => 'match_rooms#create', as: "create_match_room"
   post 'match_rooms/:id/messages/'  => 'messages#create', as: "create_message"
