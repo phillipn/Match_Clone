a7c98bcf59fac0ea8dad4d84ae09cade83670efe
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-03-29 10:44:50 -0500

    finished ranking algorithm

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,57 +3,57 @@ class UsersController < ApplicationController
 
   def index
     @match_room = MatchRoom.new
-    current_user = UserInfo.where(user_id: session[:user]['id'])
-    istj = {best: ["ESTJ", "ISTJ", "INTJ", "ISTP", "ESTP" ], ok: ["ENTJ", "INTP", "ENFJ", "INFJ", "ISFJ", "ISFP", "ENTP"], worst: ["ESFJ", "ESFP", "ENFP", "INFP"], population: 0.14}
-    istp = {best: ["ESTJ", "ISTJ", "ENTJ", "ESTP"], ok: ["ESFJ", "ISFP", "INTJ", "ISFJ"], worst: ["ISTP", "ESFP", "ENTP", "INTP", "ENFJ", "INFJ", "ENFP", 'INFP'], population: .06}
-    estp= {best: ["ISTJ", "ESTP", "ISTP", "ESFP"],ok: ["ESTJ", "ISFP", "ENTJ", "ENTP", "INTP", "ISFJ"],worst: ["ESFJ", "INTJ", "ENFJ", "INFJ", "ENFP", "INFP"],population: 0.06}
-    estj = {best: ["ISTJ", "ESFJ", "ISFJ", "ENTJ", "INTJ", "ISTP"],ok: ["ENTP", "INTP", "ESTP", "ESFP", "ISFP"],worst: ["ESTJ", "ENFJ", "INFJ", "INFP", "ENFP"],population: 0.11}
-    isfj = {best: ["ISFJ", "ENFJ", "ESTJ"],ok: ["ESFJ", "ESTP", "ISFP", "INFJ", "INFP", "ESFP", "ISTJ", "ISFP"],worst: ["ENTJ", "INTJ", "ENTP", "INTP", "ENFP"],population: .105}
-    isfp= {best: ["ESFP", "ISFP"],ok: ["ESTP", "ESTJ", "ESFJ", "ISTP", "ENFJ", "INFJ", "INFP", "ISFJ", "ISTJ", "ENFP"],worst: ["ENTJ", "INTJ", "ENTP", "INTP"],population: 0.06}
-    espf = {best: ["ESTP", "ISFP"],ok: "ESTJ", "ESFJ", "ISFJ", "ESFP", "ENTP", "ENFJ", "INFJ", "ENFP", "INFP"[],worst: ["ISTJ", "ISTP", "ENTJ", "INTJ", "INTP"],population: 0.075}
-    esfj= {best: ["ESTJ", "ENFP"],ok: ["ISFJ", "ESFJ", "ENFJ", "INFP", "ISFP", "ISTP", "ESFP"],worst: ["ESTP", "ENTJ", "INTJ", "ENTP", "INTP", "INFJ", "ISTJ"],population: 0.11}
-    infj= {best: ["ENTP", "ENFP", "INFJ", "INFP", "ENFJ"],ok: ["ISFJ", "ESFP", "ISFP", "ENTJ", "INTJ", "INTP", "ISTJ"],worst: ["ESTJ", "ESFJ", "ESTP", "ISTP"],population: 0.025}
-    infp= {best: ["ENFP", "INFP", "ENFJ", "INFJ"],ok: ["ISFJ", "ESFJ", "ESFP", "ISFP", "ENTP", "INTP"],worst: ["ESTJ", "ISTJ", "ESTP", "ISTP", "ENTJ", "INTJ"],population:  0.045}
-    enfp = {best: ["INFJ", "INFP", "ENFJ", "ENFP", "ESFJ"],ok: ["ENTJ", "ENTP", "INTJ", "INTP","ESFP", "ISFP"],worst: ["ISTJ", "ESTJ", "ISTP", "ESTP", "ISFJ"],population: 0.07}
-    enfj= {best: ["ISFJ", "ENFJ", "ENTJ", "INFJ", "ENFP", "INFP"],ok: ["ESFJ", "ESFP", "ISFP", "INTP", "ISTJ", "ENTP"],worst: ["ESTJ", "ESTP", "ISTP", "INTJ"],population: 0.04}
-    intj= {best: ["ESTJ", "INTJ", "ISTP", "ENTJ"],ok: ["INTP", "INFJ", "INFP", "ENFP"],worst: ["ESFJ", "ISFJ", "ESTP", "ESFP", "ISFP", "ENTP", "INFP", "ENFJ"],population: 0.035}
-    intp= {best: ["ENTP", "INTP", "INTJ"],ok: ["ESTJ", "ISTJ", "ESTP", "ENTJ", "ENFJ", "INFJ", "ENFP", "INFP"],worst: ["ESFJ", "ISFJ", "ISTP", "ESFP", "ISFP"],population: 0.055}
-    entp= {best: ["ENTP", "INTP", "INFJ"],ok: ["ESTJ", "ISTJ", "ESTP", "ESFP", "ENTJ", "ENFP", "INFP", "ENFJ"],worst: ["ESFJ", "ISFJ", "ISTP", "ISFP", "INTJ"],population: 0.06}
-    entj = {best: ["ESTJ", "ISTP", "ENTJ", "ENFJ", "INTJ"],ok: ["ISTJ", "ESTP", "ENTP", "INTP", "INFJ", "ENFP"],worst: ["ESFJ", "ISFJ", "ESFP", "ISFP", "INFP"],population: 0.04}
-    if current_user.personality == "ISTJ"
+    @current_user = UserInfo.where(user_id: session[:user]['id']).first
+    istj = {best: ["ESTJ", "ISTJ", "INTJ", "ISTP", "ESTP" ], ok: ["ENTJ", "INTP", "ENFJ", "INFJ", "ISFJ", "ISFP", "ENTP"], worst: ["ESFJ", "ESFP", "ENFP", "INFP"], population: 14}
+    istp = {best: ["ESTJ", "ISTJ", "ENTJ", "ESTP"], ok: ["ESFJ", "ISFP", "INTJ", "ISFJ"], worst: ["ISTP", "ESFP", "ENTP", "INTP", "ENFJ", "INFJ", "ENFP", 'INFP'], population: 6}
+    estp = {best: ["ISTJ", "ESTP", "ISTP", "ESFP"], ok: ["ESTJ", "ISFP", "ENTJ", "ENTP", "INTP", "ISFJ"],worst: ["ESFJ", "INTJ", "ENFJ", "INFJ", "ENFP", "INFP"],population: 6}
+    estj = {best: ["ISTJ", "ESFJ", "ISFJ", "ENTJ", "INTJ", "ISTP"],ok: ["ENTP", "INTP", "ESTP", "ESFP", "ISFP"],worst: ["ESTJ", "ENFJ", "INFJ", "INFP", "ENFP"],population: 11}
+    isfj = {best: ["ISFJ", "ENFJ", "ESTJ"], ok: ["ESFJ", "ESTP", "ISFP", "INFJ", "INFP", "ESFP", "ISTJ", "ISFP"],worst: ["ENTJ", "INTJ", "ENTP", "INTP", "ENFP"],population: 10.5}
+    isfp = {best: ["ESFP", "ISFP"], ok: ["ESTP", "ESTJ", "ESFJ", "ISTP", "ENFJ", "INFJ", "INFP", "ISFJ", "ISTJ", "ENFP"],worst: ["ENTJ", "INTJ", "ENTP", "INTP"],population: 6}
+    espf = {best: ["ESTP", "ISFP"], ok: ["ESTJ", "ESFJ", "ISFJ", "ESFP", "ENTP", "ENFJ", "INFJ", "ENFP", "INFP"],worst: ["ISTJ", "ISTP", "ENTJ", "INTJ", "INTP"],population: 7.5}
+    esfj = {best: ["ESTJ", "ENFP"], ok: ["ISFJ", "ESFJ", "ENFJ", "INFP", "ISFP", "ISTP", "ESFP"],worst: ["ESTP", "ENTJ", "INTJ", "ENTP", "INTP", "INFJ", "ISTJ"],population: 11}
+    infj = {best: ["ENTP", "ENFP", "INFJ", "INFP", "ENFJ"], ok: ["ISFJ", "ESFP", "ISFP", "ENTJ", "INTJ", "INTP", "ISTJ"],worst: ["ESTJ", "ESFJ", "ESTP", "ISTP"],population: 2.5}
+    infp = {best: ["ENFP", "INFP", "ENFJ", "INFJ"], ok: ["ISFJ", "ESFJ", "ESFP", "ISFP", "ENTP", "INTP"],worst: ["ESTJ", "ISTJ", "ESTP", "ISTP", "ENTJ", "INTJ"],population:  4.5}
+    enfp = {best: ["INFJ", "INFP", "ENFJ", "ENFP", "ESFJ"], ok: ["ENTJ", "ENTP", "INTJ", "INTP","ESFP", "ISFP"],worst: ["ISTJ", "ESTJ", "ISTP", "ESTP", "ISFJ"],population: 7}
+    enfj = {best: ["ISFJ", "ENFJ", "ENTJ", "INFJ", "ENFP", "INFP"],ok: ["ESFJ", "ESFP", "ISFP", "INTP", "ISTJ", "ENTP"],worst: ["ESTJ", "ESTP", "ISTP", "INTJ"],population: 4}
+    intj = {best: ["ESTJ", "INTJ", "ISTP", "ENTJ"], ok: ["INTP", "INFJ", "INFP", "ENFP"],worst: ["ESFJ", "ISFJ", "ESTP", "ESFP", "ISFP", "ENTP", "INFP", "ENFJ"],population: 3.5}
+    intp = {best: ["ENTP", "INTP", "INTJ"], ok: ["ESTJ", "ISTJ", "ESTP", "ENTJ", "ENFJ", "INFJ", "ENFP", "INFP"],worst: ["ESFJ", "ISFJ", "ISTP", "ESFP", "ISFP"],population: 5.5}
+    entp = {best: ["ENTP", "INTP", "INFJ"], ok: ["ESTJ", "ISTJ", "ESTP", "ESFP", "ENTJ", "ENFP", "INFP", "ENFJ"],worst: ["ESFJ", "ISFJ", "ISTP", "ISFP", "INTJ"],population: 6}
+    entj = {best: ["ESTJ", "ISTP", "ENTJ", "ENFJ", "INTJ"], ok: ["ISTJ", "ESTP", "ENTP", "INTP", "INFJ", "ENFP"],worst: ["ESFJ", "ISFJ", "ESFP", "ISFP", "INFP"],population: 4}
+    if @current_user.personality == "ISTJ"
       relationships = istj
-    elsif current_user.personality == "ISTP"
+    elsif @current_user.personality == "ISTP"
       relationships = istp
-    elsif current_user.personality == "ESTP"
+    elsif @current_user.personality == "ESTP"
       relationships = estp
-    elsif current_user.personality == "ESTJ"
+    elsif @current_user.personality == "ESTJ"
       relationships = estj
-    elsif current_user.personality == "ISFJ"
+    elsif @current_user.personality == "ISFJ"
       relationships = isfj
-    elsif current_user.personality == "ISFP"
+    elsif @current_user.personality == "ISFP"
       relationships = isfj
-    elsif current_user.personality == "ESPF"
+    elsif @current_user.personality == "ESPF"
       relationships = espf
-    elsif current_user.personality == "ESFJ"
+    elsif @current_user.personality == "ESFJ"
       relationships = esfj
-    elsif current_user.personality == "INFJ"
+    elsif @current_user.personality == "INFJ"
       relationships = infj
-    elsif current_user.personality == "INFP"
+    elsif @current_user.personality == "INFP"
       relationships = infg
-    elsif current_user.personality == "ENFG"
+    elsif @current_user.personality == "ENFG"
       relationships = enfg
-    elsif current_user.personality == "ENFJ"
+    elsif @current_user.personality == "ENFJ"
       relationships = enfj
-    elsif current_user.personality == "INTJ"
+    elsif @current_user.personality == "INTJ"
       relationships = intj
-    elsif current_user.personality == "INTP"
+    elsif @current_user.personality == "INTP"
       relationships = intp
-    elsif current_user.personality == "ENTP"
+    elsif @current_user.personality == "ENTP"
       relationships = entp
-    elsif current_user.personality == "ENTJ"
+    elsif @current_user.personality == "ENTJ"
       relationships = entj
     end
-    @possible_matches = UserInfo.where(Personality: ["ESTJ", "ISTJ", "INTJ", "ISTP", "ESTP", "ENTJ", "INTP", "ENFJ", "INFJ", "ISFJ", "ISFP", "ENTP","ESFJ", "ESFP", "ENFP", "INFP"])
+    @possible_matches = UserInfo.where(personality: ["ESTJ", "ISTJ", "INTJ", "ISTP", "ESTP", "ENTJ", "INTP", "ENFJ", "INFJ", "ISFJ", "ISFP", "ENTP","ESFJ", "ESFP", "ENFP", "INFP"])
     @ranking = {}
     @possible_matches.each do |match|
       if relationships[:best].include?(match.personality)
@@ -66,27 +66,27 @@ class UsersController < ApplicationController
     end
     points = 5
     @possible_matches.each do |match|
-      if current_user.smoke && match.date_smoke || !current_user.smoke
+      match_age = Date.today.year - match.birthday.year
+      match_age -= 1 if Date.today < match.birthday + match_age.years
+      if @current_user.smoke && match.date_smoke || !@current_user.smoke
         @ranking[match.user_id] += points
-      elsif current_user.kids && match.date_kids || !current_user.kids
+      elsif @current_user.kids && match.date_kids || !@current_user.kids
          @ranking[match.user_id] += points
-      elsif current_user.want_kids && match.want_kids
+      elsif @current_user.want_kids && match.want_kids
          @ranking[match.user_id] += points
-      elsif current_user.tattoo && match.date_tattoo || !current_user.tattoo
+      elsif @current_user.tattoo && match.date_tattoo || !@current_user.tattoo
          @ranking[match.user_id] += points
-      elsif current_user.date_religion && match.date_religion || !current_user.date_religion && current_user.religion == match.religion || !match.date_religion && current_user.religion == match.religion
+      elsif @current_user.date_religion && match.date_religion || !@current_user.date_religion && @current_user.religion == match.religion || !match.date_religion && @current_user.religion == match.religion
          @ranking[match.user_id] += points
-      elsif current_user.pet && match.date_pet || !current_user.pet
+      elsif @current_user.pet && match.date_pet || !@current_user.pet
          @ranking[match.user_id] += points
-      elsif current_user.date_politics && match.date_politics || !current_user.date_politics && current_user.politics == match.politics || !match.date_politics && current_user.politics == match.politics
+      elsif @current_user.date_politics && match.date_politics || !@current_user.date_politics && @current_user.politics == match.politics || !match.date_politics && @current_user.politics == match.politics
+         @ranking[match.user_id] += points
+      elsif match_age >= @current_user.min_age && match_age<= @current_user.max_age
          @ranking[match.user_id] += points
       end
     end
-
-
-#  "max_age"
-#  "min_age"
-
+    puts "RANKINGS ========== #{@ranking}"
   end
 
   def post_personality_survey
@@ -148,7 +148,63 @@ class UsersController < ApplicationController
   def post_personal_survey
     id = UserInfo.find_by(user_id: session[:user]['id'])
     id = id.id
-    info = UserInfo.update(id, :max_age => params[:min_age], :min_age => params[:max_age], :hair => params[:hair], :eye => params[:eye], :education => params[:education], :kids => params[:kids], :date_kids => params[:date_kids], :want_kids => params[:want_kids], :politics => params[:politics], :date_politics => params[:date_politics], :smoke => params[:smoke], :date_smoke => params[:date_smoke], :tattoo => params[:tattoo], :date_tattoo => params[:date_tattoo], :religion => params[:religion], :date_religion => params[:date_religion], :pet => params[:pet], :date_pet => params[:date_pet])
+    if params[:kids].downcase == 'yes'
+      params[:kids] = true
+    else
+      params[:kids] = false
+    end
+    if params[:date_kids].downcase == 'yes'
+      params[:date_kids] = true
+    else
+      params[:date_kids] = false
+    end
+    if params[:want_kids].downcase == 'yes'
+      params[:want_kids] = true
+    else
+      params[:want_kids] = false
+    end
+    if params[:date_politics].downcase == 'yes'
+      params[:date_politics] = true
+    else
+      params[:date_politics] = false
+    end
+    if params[:smoke].downcase == 'yes'
+      params[:smoke] = true
+    else
+      params[:smoke] = false
+    end
+    if params[:date_smoke].downcase == 'yes'
+      params[:date_smoke] = true
+    else
+      params[:date_smoke] = false
+    end
+    if params[:tattoo].downcase == 'yes'
+      params[:tattoo] = true
+    else
+      params[:tattoo] = false
+    end
+    if params[:date_tattoo].downcase == 'yes'
+      params[:date_tattoo] = true
+    else
+      params[:date_tattoo] = false
+    end
+    if params[:date_religion].downcase == 'yes'
+      params[:date_religion] = true
+    else
+      params[:date_religion] = false
+    end
+    if params[:pet].downcase == 'yes'
+      params[:pet] = true
+    else
+      params[:pet] = false
+    end
+    if params[:date_pet].downcase == 'yes'
+      params[:date_pet] = true
+    else
+      params[:date_pet] = false
+    end
+    puts "updated params #{params}"
+    info = UserInfo.update(id, :max_age => params[:min_age], :min_age => params[:max_age], :hair => params[:hair], :eye => params[:eye], :education => params[:education], :kids => params[:kids], :date_kids => params[:date_kids], :want_kids => params[:want_kids], :politics => params[:politics], :date_politics => params[:date_politics], :smoke => params[:smoke], :date_smoke => params[:date_smoke], :tattoo => params[:tattoo], :date_tattoo => params[:date_tattoo], :religion => params[:religion], :date_religion => params[:date_religion], :pet => params[:pet], :date_pet => params[:date_pet], :birthday => params[:birthday])
     if info.errors.full_messages[0]
      flash[:errors] = info.errors.full_messages
      redirect_to '/survey/personal'
