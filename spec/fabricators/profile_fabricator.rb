0dc6d2e1f3c300c1392df1ade4755f6cea341449
Pat Falconer <Patrick.j.Falconer@gmail.com>
2017-03-31 12:48:39 -0500

    style changes

--- spec/fabricators/profile_fabricator.rb
+++ spec/fabricators/profile_fabricator.rb
@@ -2,6 +2,6 @@ Fabricator(:profile) do
   religion { %w(Christianity Islam Judaism Buddhism Hinduism Atheistism Mormonism Other).sample }
   about_me { Faker::Lorem.paragraph }
   ideal_mate { Faker::Lorem.sentence }
-  job { Faker::Lorem.sentence }
+  job { Faker::Lorem.word }
   hobbies { Faker::Lorem.sentence }
 end
